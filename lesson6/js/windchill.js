6612237e60a0ac551cc5d6821b17ef39e731a8e3
speelbergo <speelbergo@gmail.com>
2021-05-24 18:08:16 -0600

    lesson 5 minor fix

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,9 @@
+
+
+
+
+//Create a "windchill.js" file and add it to your "script" folder in the "lesson-6" folder.
+// The "windchill.js" script should get the two input values of temperature and wind speed,
+// check to make sure they meet the required values allowed to officially calculate the wind chill, 
+//and then either calculate and display the windchill factor value or display "N/A" (not applicable) 
+//if the input values did not meet the requirements.
