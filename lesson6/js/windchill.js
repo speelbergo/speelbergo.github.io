c68591318576141fa79741946f8d92e4576fb592
speelbergo <speelbergo@gmail.com>
2021-05-27 15:38:39 -0600

    validate HTML i think

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,8 +1,11 @@
+let t = document.getElementById("temph").innerHTML;
+let s = document.getElementById("wendy").innerHTML;
 
 
+let windChill =  (35.74 + (.6215 * t) - (35.75 * (Math.pow(s, .16))) + (.4275 * t * (Math.pow(s, .16)) ) )
 
+document.getElementById("wchill").innerHTML = windChill;
 
-//Create a "windchill.js" file and add it to your "script" folder in the "lesson-6" folder.
 // The "windchill.js" script should get the two input values of temperature and wind speed,
 // check to make sure they meet the required values allowed to officially calculate the wind chill, 
 //and then either calculate and display the windchill factor value or display "N/A" (not applicable) 
