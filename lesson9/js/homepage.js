bbd49825229fae83b680d627fd0b88b16da6bece
speelbergo <speelbergo@gmail.com>
2021-06-18 08:35:04 -0600

    week 09 in progress

--- lesson9/js/homepage.js
+++ lesson9/js/homepage.js
@@ -9,9 +9,9 @@ fetch(requestURL)
 
 const towns = jsonObject["towns"];
 
-for (let i = 0; i < towns.length; i++) { 
+const threetowns = towns.filter(town => town.name == "Preston" || town.name == "Soda Springs" || town.name == "Fish Haven");
 
-if ((towns[i]) == 1) { 
+threetowns.forEach(towns => { 
 
 let card = document.createElement("section");
 let title = document.createElement("h2");
@@ -19,17 +19,29 @@ let mottoh = document.createElement ("p");
 let founded = document.createElement("p");
 let population = document.createElement("p");
 let annual = document.createElement("p");
+let pix = document.createElement("img");
+let carddiv = document.createElement("div");
 
 title.textContent = towns.name;
 mottoh.textContent = towns.motto;
+founded.textContent = "Founded in " + towns.yearFounded;
+population.textContent = "Population: " + towns.currentPopulation;
+annual.textContent = "Annual Rainfall: " + towns.averageRainfall;
+pix.setAttribute('src', `images/${towns.photo}`);
+pix.setAttribute('alt', `The colony of ${towns.name}!`);
+pix.className = "cardimg";
+carddiv.className = "cardtext";
 
+carddiv.appendChild(title);
+carddiv.appendChild(mottoh);
+carddiv.appendChild(founded);
+carddiv.appendChild(population);
+carddiv.appendChild(annual);
 
-card.appendChild(title);
-card.appendChild(mottoh);
+card.appendChild(pix);
+card.appendChild(carddiv);
+document.querySelector(".card1").appendChild(card);
 
-document.querySelector("div.card1").appendChild(card);
-
-}
-}
+})
 
   });
